4977e2e26ae046f6bef5b02f09638b90f540a9fb
doitwithnotepad <doitwithnotepad@proton.me>
2024-11-17 14:43:43 +0000

    Random

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,11 @@ export default defineConfig({
 		sveltekit(),
 		viteStaticCopy({
 			targets: [
-				{ src: 'tower.ico', dest: '' },
+				{ src: 'favicon.ico', dest: '' },
 				{ src: 'scrollbar.css', dest: '' },
 				{ src: 'serviceWorker.js', dest: '' },
 				{ src: 'login.html', dest: '' },
-				{ src: 'assets/', dest: '' },
-				{ src: 'documents/', dest: '' }
+				{ src: 'assets/', dest: '' }
 			]
 		})
 	]
