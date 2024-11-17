d7fac19bc38b6086c0287e598862cb77234378b8
doitwithnotepad <doitwithnotepad@proton.me>
2024-11-17 14:50:02 +0000

    Last one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,4 @@ function handleProcessCreated(processCount)
 }
 </script>
 
-<WebVM configObj={configObj} processCallback={handleProcessCreated} cacheId="blocks_terminal">
-	<p>Looking for a complete desktop experience? Try the new <a class="underline" href="/alpine.html" target="_blank">Alpine Linux</a> graphical WebVM</p>
-</WebVM>
+<WebVM configObj={configObj} processCallback={handleProcessCreated} cacheId="blocks_terminal"></WebVM>
